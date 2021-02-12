a7e579ad136f3b0c7f8ad465ddd0a72491cb2616
Rodrigo de Melo <rodrigoeddie@gmail.com>
2021-02-11 21:47:01 -0300

    Working...

--- build/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CardProject } from '../../src/components/CardProject.vue'
-export { default as Header } from '../../src/components/Header.vue'
-
-export const LazyCardProject = import('../../src/components/CardProject.vue' /* webpackChunkName: "components/card-project" */).then(c => c.default || c)
-export const LazyHeader = import('../../src/components/Header.vue' /* webpackChunkName: "components/header" */).then(c => c.default || c)
